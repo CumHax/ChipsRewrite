cc22d95954d8ce9f9f868d6657bb4f08809d5de7
CumHax <83083809+CumHax@users.noreply.github.com>
2021-12-19 12:14:29 +0300

    Update Client.java

--- src/main/java/me/cumhax/chipshack/Client.java
+++ src/main/java/me/cumhax/chipshack/Client.java
@@ -3,6 +3,8 @@ package me.cumhax.chipshack;
 import me.cumhax.chipshack.event.EventHandler;
 import me.cumhax.chipshack.command.CommandManager;
 import me.cumhax.chipshack.gui.clickgui.ClickGUI;
+import me.cumhax.chipshack.gui.clickgui2.ClickGUI2;
+import me.cumhax.chipshack.gui.clickgui2.font.CustomFontRenderer;
 import me.cumhax.chipshack.manager.FriendManager;
 import me.cumhax.chipshack.module.ModuleManager;
 import me.cumhax.chipshack.setting.SettingManager;
@@ -15,13 +17,15 @@ import me.cumhax.chipshack.manager.ConfigManager;
 import java.awt.*;
 import java.io.IOException;
 
-@Mod(modid = "chipshack", name = "ChipsHack", version = "b3")
+@Mod(modid = "chipshack", name = "ChipsHack", version = "0.3")
 public class Client
 {
 	public static ModuleManager moduleManager;
 	public static SettingManager settingManager;
 	public static CustomFontRenderer customFontRenderer;
 	public static ClickGUI clickGUI;
+	public static CustomFontRenderer2 customFontRenderer2;
+	public static ClickGUI2 clickGUI2;
 	public static CommandManager commandManager;
 	public static FriendManager friendManager;
 
@@ -31,8 +35,10 @@ public class Client
 		settingManager = new SettingManager();
 		moduleManager = new ModuleManager();
 		friendManager = new FriendManager();
+		customFontRenderer2 = new CustomFontRenderer2(new Font("Arial", Font.PLAIN, 19), true, false);
 		customFontRenderer = new CustomFontRenderer(new Font("Verdana", Font.PLAIN, 19), true, false);
 		clickGUI = new ClickGUI();
+		clickGUI2 = new ClickGUI2();
 
 		ConfigManager.loadConfig();
 
