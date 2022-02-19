fbab1729d3eb93984fe8d8f4b5f213e4221a7d94
CumHax <83083809+CumHax@users.noreply.github.com>
2022-02-19 23:23:51 +0300

    Update ModuleButton.java

--- src/main/java/me/cumhax/chipshack/gui/clickgui/button/ModuleButton.java
+++ src/main/java/me/cumhax/chipshack/gui/clickgui/button/ModuleButton.java
@@ -76,6 +76,15 @@ public class ModuleButton
 			{
 				switch (Client.settingManager.getSetting("ClickGUI", "Color").getEnumValue())
 				{
+                                       	case "Red":
+						Client.clickGUI.drawGradient(X, Y, X + W , Y + H, new Color(220, 30, 30, 232).getRGB(), new Color(216, 30, 30, 232).getRGB());
+						break;
+					case "Green":
+						Client.clickGUI.drawGradient(X, Y, X + W, Y + H, new Color(30, 220, 30, 232).getRGB(), new Color(30, 216, 30, 232).getRGB());
+						break;
+					case "Blue":
+						Client.clickGUI.drawGradient(X, Y, X + W, Y + H, new Color(30, 30, 220, 232).getRGB(), new Color(30, 30, 216, 232).getRGB());
+						break;
 					case "Purple":
 						Client.clickGUI.drawGradient(X, Y, X + W, Y + H, new Color(140, 0, 180, 232).getRGB(), new Color(136, 0, 180, 232).getRGB());
 						break;
@@ -87,6 +96,15 @@ public class ModuleButton
 			{
 				switch (Client.settingManager.getSetting("ClickGUI", "Color").getEnumValue())
 				{
+                                       	case "Red":
+						Client.clickGUI.drawGradient(X, Y, X + W , Y + H, new Color(220, 30, 30, 232).getRGB(), new Color(216, 30, 30, 232).getRGB());
+						break;
+					case "Green":
+						Client.clickGUI.drawGradient(X, Y, X + W, Y + H, new Color(30, 220, 30, 232).getRGB(), new Color(30, 216, 30, 232).getRGB());
+						break;
+					case "Blue":
+						Client.clickGUI.drawGradient(X, Y, X + W, Y + H, new Color(30, 30, 220, 232).getRGB(), new Color(30, 30, 216, 232).getRGB());
+						break;
 					case "Purple":
 						Client.clickGUI.drawGradient(X, Y, X + W , Y + H, new Color(150, 0, 180, 232).getRGB(), new Color(146, 0, 180, 232).getRGB());
 						break;
