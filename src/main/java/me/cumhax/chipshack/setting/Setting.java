9d1c874ee5c3a7f346b3510f445fddd7ae92d93c
CumHax <83083809+CumHax@users.noreply.github.com>
2022-02-26 13:30:25 +0300

    Update Setting.java

--- src/main/java/me/cumhax/chipshack/setting/Setting.java
+++ src/main/java/me/cumhax/chipshack/setting/Setting.java
@@ -4,124 +4,105 @@ import me.cumhax.chipshack.module.Module;
 
 import java.util.List;
 
-public class Setting
-{
-	private final String name;
-	private final Module module;
-	private final SettingType type;
-	private boolean booleanValue;
-	private int integerValue;
-	private int minIntegerValue;
-	private int maxIntegerValue;
-        private int IntValue;
-        private boolean boolValue;
-	private String enumValue;
-	private List<String> enumValues;
+public class Setting {
+    private final String name;
+    private final Module module;
+    private final SettingType type;
+    private boolean booleanValue;
+    private boolean boolValue;
+    private int integerValue;
+    private int minIntegerValue;
+    private int maxIntegerValue;
+    private int IntValue;
+    private String enumValue;
+    private List<String> enumValues;
 
-	public Setting(String name, Module module, int intValue, int intMinValue, int intMaxValue)
-	{
-		this.name = name;
-		this.module = module;
-		this.integerValue = intValue;
-                this.IntValue = intValue;
-		this.minIntegerValue = intMinValue;
-		this.maxIntegerValue = intMaxValue;
-		this.type = SettingType.INTEGER;
-	}
+    public Setting(String name, Module module, int intValue, int intMinValue, int intMaxValue) {
+        this.name = name;
+        this.module = module;
+        this.integerValue = intValue;
+        this.IntValue = intValue;
+        this.minIntegerValue = intMinValue;
+        this.maxIntegerValue = intMaxValue;
+        this.type = SettingType.INTEGER;
+    }
 
-	public Setting(String name, Module module, boolean boolValue)
-	{
-		this.name = name;
-		this.module = module;
-		this.booleanValue = boolValue;
-		this.type = SettingType.BOOLEAN;
-	}
+    public Setting(String name, Module module, boolean boolValue) {
+        this.name = name;
+        this.module = module;
+        this.booleanValue = boolValue;
+        this.type = SettingType.BOOLEAN;
+    }
 
-	public Setting(String name, Module module, List<String> enumValues)
-	{
-		this.name = name;
-		this.module = module;
-		this.enumValue = enumValues.get(0);
-		this.enumValues = enumValues;
-		this.type = SettingType.ENUM;
-	}
+    public Setting(String name, Module module, List<String> enumValues) {
+        this.name = name;
+        this.module = module;
+        this.enumValue = enumValues.get(0);
+        this.enumValues = enumValues;
+        this.type = SettingType.ENUM;
+    }
 
-	public String getName()
-	{
-		return name;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public Module getModule()
-	{
-		return module;
-	}
+    public Module getModule() {
+        return this.module;
+    }
 
-	public SettingType getType()
-	{
-		return type;
-	}
+    public SettingType getType() {
+        return this.type;
+    }
 
-	public boolean getBooleanValue()
-	{
-		return booleanValue;
-	}
+    public boolean getBooleanValue() {
+        return this.booleanValue;
+    }
 
-	public void setBooleanValue(boolean booleanValue)
-	{
-		this.booleanValue = booleanValue;
-	}
+    public void setBooleanValue(boolean booleanValue) {
+        this.booleanValue = booleanValue;
+    }
 
-	public int getIntegerValue()
-	{
-		return integerValue;
-	}
+    public int getIntegerValue() {
+        return this.integerValue;
+    }
 
-	public void setIntegerValue(int integerValue)
-	{
-		this.integerValue = integerValue;
-	}
+    public void setIntegerValue(int integerValue) {
+        this.integerValue = integerValue;
+    }
 
-	public int getMinIntegerValue()
-	{
-		return minIntegerValue;
-	}
-        public int getIntValue() 
-        {
-                return this.IntValue;
-        }
-        public boolean getBoolValue() 
-        {
-                return boolValue;
-        }
-        public string getEnumValue() 
-        {
+    public int getMinIntegerValue() {
+        return this.minIntegerValue;
+    }
+
+    public int getIntValue() {
+        return this.IntValue;
+    }
+
+    public boolean getBoolValue() {
+        return boolValue;
+    }
+
+    public void setBoolValue(boolean boolValue) {
+        this.boolValue = boolValue;
+    }
+
+    public int getMaxIntegerValue() {
+        return this.maxIntegerValue;
+    }
+
+    public String getEnumValue() {
         return this.enumValue;
-        }
+    }
 
+    public Object getValue() {
+        return null;
+    }
 
-	public int getMaxIntegerValue()
-	{
-		return maxIntegerValue;
-	}
+    public void setEnumValue(String enumValue) {
+        this.enumValue = this.enumValues.contains(enumValue) ? enumValue : this.enumValue;
+    }
 
-	public boolean isInteger() { return type.equals("int"); }
-
-	public boolean isBoolean() { return type.equals("boolean"); }
-
-	public boolean isEnum() { return type.equals("enum"); }
-
-	public String getEnumValue()
-	{
-		return enumValue;
-	}
-
-	public void setEnumValue(String enumValue)
-	{
-		this.enumValue = enumValues.contains(enumValue)? enumValue : this.enumValue; // only change value if list includes it.
-	}
-
-	public List<String> getEnumValues()
-	{
-		return enumValues;
-	}
+    public List<String> getEnumValues() {
+        return this.enumValues;
+    }
 }
