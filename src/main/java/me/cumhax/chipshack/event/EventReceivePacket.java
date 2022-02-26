8a4042244e2ba5f741236fb693b45104425bfc28
CumHax <83083809+CumHax@users.noreply.github.com>
2022-02-27 00:30:40 +0300

    Create EventReceivePacket.java

---
+++ src/main/java/me/cumhax/chipshack/event/EventReceivePacket.java
@@ -0,0 +1,20 @@
+package me.cumhax.chipshack.event;
+
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraft.network.Packet;
+
+public class EventReceivePacket extends Event {
+    private Packet packet;
+
+    public EventReceivePacket(Packet packet) {
+        this.packet = packet;
+    }
+
+    public Packet getPacket() {
+        return packet;
+    }
+    public void setPacket(Packet packet) {
+        this.packet = packet;
+    }
+}
