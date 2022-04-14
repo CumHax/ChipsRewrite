b83afbf6c0bb247ec13d98c3e92979a8df1b15a9
CumHax <83083809+CumHax@users.noreply.github.com>
2022-04-14 18:38:51 +0300

    Create ChatNotify.java

---
+++ src/main/java/me/cumhax/chipshack/module/chat/ChatNotify.java
@@ -0,0 +1,25 @@
+package me.cumhax.chipshack.module.chat;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+import me.cumhax.chipshack.event.ModuleToggleEvent;
+import me.cumhax.chipshack.module.Module;
+import me.cumhax.chipshack.module.Category;
+import me.cumhax.chipshack.util.LoggerUtil;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class ChatNotify extends Module {
+    public ChatNotify() {
+        super("ChatNotify", "Sends a message when you toggle a module.", Category.CHAT);
+        this.setNeedsKeybind(false);
+    }
+
+    @SubscribeEvent
+    public void onModuleEnable(ModuleToggleEvent.Pre event) {
+        LoggerUtil.sendMessage((Object)ChatFormatting.BOLD + event.getModule().getName() + (Object)ChatFormatting.GREEN + " ENABLED!");
+    }
+
+    @SubscribeEvent
+    public void onModuleDisable(ModuleToggleEvent.Post event) {
+        LoggerUtil.sendMessage((Object)ChatFormatting.BOLD + event.getModule().getName() + (Object)ChatFormatting.RED + " DISABLED!");
+    }
+}
